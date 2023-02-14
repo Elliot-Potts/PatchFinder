38a46781564e408d0f49f7ccc6b7a6169af411dd
Elliot <elliot.potts@northumbria.ac.uk>
2023-02-14 20:43:25 +0000

    Added some initial connection logic
    Netmiko connection handling is working.

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from netmiko import ConnectHandler, exceptions
+from ciscoconfparse import CiscoConfParse
 from dotenv import load_dotenv
 from pprint import pprint
 import time
 import os
 
 
+def handle_connection(switch_ip):
+    load_dotenv()
+    return ConnectHandler(host=switch_ip, username=os.environ.get("S_USERNAME"), password=os.environ.get("S_PASSWORD"), device_type="cisco_ios")
+
+
+def main():
+    get_ip_address = input("Enter switch IP: ")
+    switch_connect = handle_connection(get_ip_address)
+
+    int_status = switch_connect.send_command("sh int status", use_textfsm=True)
+
+    unconnected_switchports = []
+
+    for interface in int_status:
+        if interface['status'] == "notconnect":
+            print("Interface [ {sp} ] is notconnect.".format(sp=interface['port']))
+            unconnected_switchports.append(interface)
+
+
+if __name__ == "__main__":
+    main()
+    print("PatchFinder Done")
 
