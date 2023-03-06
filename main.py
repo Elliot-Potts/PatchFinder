4247993e5b7b7b33aaa23bce081edcf2b19fc177
Elliot Potts <elliot.potts@northumbria.ac.uk>
2023-03-06 15:49:40 +0000

    Added support for fetching PoE details
    Added custom parsing for #show power inline - working on 2960 standalone and stacks as well as 9200L.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 """
 TODO
-- Add POE info, budget etc.
--- No TextFSM support, need to test reliability of #show power on stack switches
 - Remove duplicate ValueError handling for management interfaces
+- Test this program against Cisco 9200L switches
 """
 
 from netmiko import ConnectHandler, exceptions
@@ -40,6 +39,22 @@ def auth_handler(ip_addresses):
 
 
 def main(ip_address):
+    test_power_2960 = """Module   Available     Used     Remaining
+          (Watts)     (Watts)    (Watts)
+------   ---------   --------   ---------
+1           740.0      404.0       336.0
+2           740.0       45.8       694.2
+3             n/a        n/a         n/a
+4             n/a        n/a         n/a
+5           740.0      120.8       619.2
+Interface Admin  Oper       Power   Device              Class Max
+                            (Watts)
+--------- ------ ---------- ------- ------------------- ----- ----
+Gi1/0/1   auto   on         30.0    AIR-AP3802I-E-K9    4     30.0
+Gi1/0/2   auto   on         30.0    AIR-AP3802I-E-K9    4     30.0
+Gi1/0/3   auto   on         30.0    AIR-AP3802I-E-K9    4     30.0
+"""  # Power inline parsing debug
+
     try:
         switch_connection = ConnectHandler(
             host=ip_address,
@@ -57,8 +72,44 @@ def main(ip_address):
     switch_hostname = switch_connection.send_command("sh run | include hostname").split()[1]
     rich_console.print("[bold green][+][/bold green] Connected to {ip}  ([italic green]{hostn}[/])\n".format(ip=ip_address, hostn=switch_hostname))
     switch_uptime = switch_connection.send_command("sh version", use_textfsm=True)[0]['uptime']
-    switch_power = switch_connection.send_command("sh power", use_textfsm=True).split()
+    switch_power = switch_connection.send_command("sh power inline", use_textfsm=True).replace("-", "").split()
+    # switch_power = test_power_2960.replace("-", "").split()  # Power inline parsing debug
     int_status = switch_connection.send_command("sh int status", use_textfsm=True)
+
+    switch_power_parsed = []
+
+    for item in range(7, switch_power.index("Interface")):
+        if switch_power[item].isdigit():
+            switch_power_parsed.append(switch_power[item:item+4])
+        
+    if not switch_power_parsed:
+        if switch_power[0].startswith("Available") and switch_power[1].startswith("Used") and switch_power[2].startswith("Remaining"):
+            get_available = switch_power[0].split(":")[1].replace("(w)", "")
+            get_used = switch_power[1].split(":")[1].replace("(w)", "")
+            get_remaining = switch_power[2].split(":")[1].replace("(w)", "")
+            switch_power_parsed.append(["System Total", get_available, get_used, get_remaining])
+        else:
+            rich_console.print(f"[bold red][-][/] Unable to fetch PoE details for ({ip_address}).")
+
+    if len(switch_power_parsed) > 0:
+        poe_table = Table(show_header=True, header_style="bold white")
+        poe_table.add_column("Switch No.")    
+        poe_table.add_column("Available")
+        poe_table.add_column("Used")
+        poe_table.add_column("Free")
+
+        for switch in switch_power_parsed:
+            if switch[3] == "n/a" or switch[3] == "0.0":
+                poe_free = f"[red]{switch[3]}[/]"
+            else:
+                poe_free = switch[3]
+
+            poe_table.add_row(
+                switch[0],
+                switch[1],
+                switch[2],
+                poe_free
+            )
     
     all_stats = []
     unconnected_switchports = {}
@@ -86,7 +137,7 @@ def main(ip_address):
     table.add_column("Last Input")
     table.add_column("Input Packets")
     table.add_column("Output Packets")
-    table.add_column("Difference (%)")
+    table.add_column("Percentage Use (%)")
 
     for dc_switchport in unconnected_switchports:
         # Dict ?
@@ -121,9 +172,12 @@ def main(ip_address):
     interface_percentages = sorted(interface_percentages, key=lambda x: x[0])
     
     rich_console.print(table)
-    rich_console.print(Panel.fit("Switch uptime is: [bold]{}[/]".format(switch_uptime)))
 
-    # print(switch_power)  # TODO Test reliability of this array on STACK switches, could break, if not .split()[-1]
+    if len(switch_power_parsed) > 0:
+        rich_console.print("[bold]PoE Details[/]")
+        rich_console.print(poe_table)
+
+    rich_console.print(Panel.fit("Switch uptime is: [bold]{}[/]".format(switch_uptime)))
 
     rich_console.print("\nInterface [bold green] {int} [/] has [bold green] {usage}% [/] the usage of the highest on the switch.\n".format(
         int=interface_percentages[0][1],
