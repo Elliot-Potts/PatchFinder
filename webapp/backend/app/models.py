22c27d260770a7b10e6f6973db3c5a627281d671
Elliot Potts <elliotepotts@gmail.com>
2025-06-17 16:33:30 +0100

    Began Docker setup

--- webapp/backend/app/models.py
+++ webapp/backend/app/models.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./users.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////app/app/users.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
